b3e83702fd17453681ceea585746cc6c6c941b4b
kai_hirsinger <kai.hirsinger@gmail.com>
2016-08-02 12:42:19 +1000

    added proper scoping to metric_group and mode_util

--- metric_group.js
+++ metric_group.js
@@ -1,84 +1,75 @@
-/* Objects */
+( function (exports) {
 
-function MetricGroup (title, metrics, container, labelled) {
-  this.title     = title;
-  this.container = container;
-  this.metrics   = [];
-  for (var label in metrics) {
-    if (labelled) {
-      var metric = this._createMetric(metrics[label], label);
-    } else {
-      var metric = this._createMetric(metrics[label])
+  /* Objects */
+
+  function MetricGroup (title, metrics, container, labelled) {
+    this.title     = title;
+    this.container = container;
+    this.metrics   = [];
+    for (var label in metrics) {
+      if (labelled) {
+        var metric = this._createMetric(metrics[label], label);
+      } else {
+        var metric = this._createMetric(metrics[label])
+      }
+      this.metrics.push(metric);
     }
-    this.metrics.push(metric);
-  }
 
-  var row = document.createElement("div");
-  row.setAttribute("class", "row");
+    var row = document.createElement("div");
+    row.setAttribute("class", "row");
 
-  var header = document.createElement("p");
-  header.setAttribute("class", "metric-header");
-  header.appendChild(document.createTextNode(this.title));
-  row.appendChild(header);
-
-  this.metrics.forEach( function(element) {
-    row.appendChild(element);
-  });
-
-  this.element = row;
-}
-
-MetricGroup.prototype._createMetric = function(value, label) {
-  var metricElement = document.createElement("div");
-  metricElement.setAttribute("class", "metric-container");
-  if (label) {
     var header = document.createElement("p");
-    var headerText = label.replace(/_/g, " ");
-    header.setAttribute("class", "metric-sub-header");
-    header.appendChild(document.createTextNode(headerText));
-    metricElement.appendChild(header);
+    header.setAttribute("class", "metric-header");
+    header.appendChild(document.createTextNode(this.title));
+    row.appendChild(header);
+
+    this.metrics.forEach( function(element) {
+      row.appendChild(element);
+    });
+
+    this.element = row;
   }
 
-  var content = document.createElement("p");
-  var contentValue = numberWithSpaces(value);
-  content.setAttribute("class", "metric-value");
-  content.appendChild(document.createTextNode(contentValue));
-  metricElement.appendChild(content);
+  MetricGroup.prototype._createMetric = function(value, label) {
+    var metricElement = document.createElement("div");
+    metricElement.setAttribute("class", "metric-container");
+    if (label) {
+      var header = document.createElement("p");
+      var headerText = label.replace(/_/g, " ");
+      header.setAttribute("class", "metric-sub-header");
+      header.appendChild(document.createTextNode(headerText));
+      metricElement.appendChild(header);
+    }
 
-  return metricElement;
-}
+    var content = document.createElement("p");
+    var contentValue = numberWithSpaces(value);
+    content.setAttribute("class", "metric-value");
+    content.appendChild(document.createTextNode(contentValue));
+    metricElement.appendChild(content);
 
-MetricGroup.prototype.create = function() {
-  this.container.appendChild(this.element);
-}
+    return metricElement;
+  }
+
+  MetricGroup.prototype.create = function() {
+    this.container.appendChild(this.element);
+  }
 
 
-/* Helper Functions */
+  /* Helper Functions */
 
-function numberWithSpaces(number) {
-  return number
-    .toString()
-    .replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-}
+  function numberWithSpaces(number) {
+    return number
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  }
 
-function getQueryResultsByName(name) {
-  return datasets.filter(function(d) {
-    return d.queryName == name;
-  })[0].content;
-}
 
-function sumDataColumn(data, columnName) {
-  return data
-    .map(function(d) {
-      return d[columnName];
-    })
-    .reduce(function(acc, curr) {
-      return acc + curr;
-    });
-}
+  /* Interface */
 
-function createMetricGroup(title, data, containerId, labelled) {
-  var container = document.getElementById(containerId);
-  var metrics   = new MetricGroup(title, data, container, labelled);
-  metrics.create();
-}
+  exports.createMetricGroup = function (title, data, containerId, labelled) {
+    var container = document.getElementById(containerId);
+    var metrics   = new MetricGroup(title, data, container, labelled);
+    metrics.create();
+  };
+
+} ) ( this.metric_group = {} );
